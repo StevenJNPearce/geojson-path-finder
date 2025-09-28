2e69d57c977461b014fd645164b9d707ee18c35d
StevenJNPearce <stevenjnpearce@gmail.com>
2025-09-28 05:33:45 +0100

    Preserve Z dimension when rounding coordinates

--- src/round-coord.ts
+++ src/round-coord.ts
@@ -4,8 +4,14 @@ export default function roundCoord(
   coord: Position,
   tolerance: number
 ): Position {
-  return [
+  const rounded: Position = [
     Math.round(coord[0] / tolerance) * tolerance,
     Math.round(coord[1] / tolerance) * tolerance,
   ];
+
+  for (let i = 2; i < coord.length; i++) {
+    rounded[i] = coord[i];
+  }
+
+  return rounded;
 }
